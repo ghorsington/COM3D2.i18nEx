7db2a200d386b340a516131c5f9be8b7d5243c13
ghorsington <neigh@coder.horse>
2019-07-28 12:11:10 +0300

    Initial commit

---
+++ COM3D2.i18nEx.Core/InternationalizationCore.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace COM3D2.i18nEx.Core
+{
+    public class InternationalizationCore
+    {
+    }
+}

064a321a58f7da9581130d3e9b83e72b4c3c3659
ghorsington <neigh@coder.horse>
2019-07-28 17:53:15 +0300

    Add configuration and paths

---
+++ COM3D2.i18nEx.Core/ConfigWrapper.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.ComponentModel;
+using System.Linq;
+using ExIni;
+
+namespace COM3D2.i18nEx.Core
+{
+    internal class ConfigWrapper<T>
+    {
+        private readonly IniFile file;
+        private readonly string savePath;
+        private string section, key, description;
+        private readonly IniKey iniKey;
+        private readonly TypeConverter cvt;
+
+        public ConfigWrapper(IniFile file, string savePath, string section, string key, string description = null,
+            T defaultValue = default)
+        {
+            this.file = file;
+            this.savePath = savePath;
+            this.section = section;
+            this.key = key;
+            this.description = description;
+
+            iniKey = file[section][key];
+            iniKey.Comments.Comments = description?.Split('\n').ToList();
+
+            cvt = TypeDescriptor.GetConverter(typeof(T));
+
+            if (!cvt.CanConvertFrom(typeof(string)))
+                throw new ArgumentException("Default TypeConverter can't convert from String");
+
+            if (!cvt.CanConvertTo(typeof(string)))
+                throw new ArgumentException("Default TypeConverter can't convert to String");
+
+            if (iniKey.Value == null)
+                Value = defaultValue;
+        }
+
+        public T Value
+        {
+            get => (T) cvt.ConvertFromInvariantString(iniKey.Value);
+            set
+            {
+                iniKey.Value = cvt.ConvertToInvariantString(value);
+                Save();
+            }
+        }
+
+        private void Save()
+        {
+            file.Save(savePath);
+        }
+    }
+}
---
+++ COM3D2.i18nEx.Core/Configuration.cs
@@ -0,0 +1,43 @@
+﻿using System.IO;
+using ExIni;
+
+namespace COM3D2.i18nEx.Core
+{
+    internal static class Configuration
+    {
+        private static readonly IniFile configFile;
+
+        static Configuration()
+        {
+            configFile = File.Exists(Paths.ConfigurationFilePath) ? IniFile.FromFile(Paths.ConfigurationFilePath) : new IniFile();
+        }
+
+        public static void Reload()
+        {
+            configFile.Merge(IniFile.FromFile(Paths.TranslationsRoot));
+        }
+
+        private static ConfigWrapper<T> Wrap<T>(string section, string key, string description = "", T @default = default)
+        {
+            return new ConfigWrapper<T>(configFile, Paths.ConfigurationFilePath, section, key, description, @default);
+        }
+
+        public static ConfigWrapper<int> MaxTranslationFilesCached = Wrap(
+            "TextTranslations", 
+            "CacheSize",
+            "Specifies how many text translation files should be kept in memory at once\nHaving bigger cache can improve performance at the cost of memory usage",
+            1);
+    }
+
+    internal static class Paths
+    {
+        public static string TranslationsRoot { get; private set; }
+        public static string ConfigurationFilePath { get; private set; }
+
+        public static void Initialize(string gameRoot)
+        {
+            TranslationsRoot = Path.Combine(gameRoot, "i18nEx");
+            ConfigurationFilePath = Path.Combine(TranslationsRoot, "configuration.ini");
+        }
+    }
+}

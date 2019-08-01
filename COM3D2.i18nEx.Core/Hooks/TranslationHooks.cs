b7b41c934cf11c3a30bac4391952280674fc69a8
ghorsington <neigh@coder.horse>
2019-08-01 21:08:20 +0300

    Fix config scene buttons not being displayed

--- COM3D2.i18nEx.Core/Hooks/TranslationHooks.cs
+++ COM3D2.i18nEx.Core/Hooks/TranslationHooks.cs
@@ -1,4 +1,7 @@
-﻿using BepInEx.Harmony;
+﻿using System.Collections.Generic;
+using System.Reflection;
+using System.Reflection.Emit;
+using BepInEx.Harmony;
 using HarmonyLib;
 
 namespace COM3D2.i18nEx.Core.Hooks
@@ -33,6 +36,17 @@ namespace COM3D2.i18nEx.Core.Hooks
         private static void IsJapan(ref bool __result)
         {
             __result = false;
+        }
+
+        [HarmonyPatch(typeof(SceneNetorareCheck), "Start")]
+        [HarmonyTranspiler]
+        private static IEnumerable<CodeInstruction> FixNTRCheckScene(IEnumerable<CodeInstruction> instructions)
+        {
+            foreach (var ins in instructions)
+                if (ins.opcode == OpCodes.Call && ins.operand is MethodInfo minfo && minfo.Name == "get_isJapan")
+                    yield return new CodeInstruction(OpCodes.Ldc_I4_1);
+                else
+                    yield return ins;
         }
     }
 }

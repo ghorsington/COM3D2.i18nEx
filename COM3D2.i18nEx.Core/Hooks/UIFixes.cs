8c0566e527d9dde0cd81247237514b07d32af5a1
ghorsington <neigh@coder.horse>
2019-08-11 21:56:29 +0300

    Add ability to translate the game info dialog

--- COM3D2.i18nEx.Core/Hooks/UIFixes.cs
+++ COM3D2.i18nEx.Core/Hooks/UIFixes.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection;
+using System.Reflection.Emit;
 using System.Text;
 using BepInEx.Harmony;
 using HarmonyLib;
@@ -29,10 +31,11 @@ namespace COM3D2.i18nEx.Core.Hooks
         public static void LocalizeNTRScene(GameObject ___toggleParent)
         {
             Core.Logger.LogInfo("Fixing NTR check scene.");
+
             void Localize(string item)
             {
-                var result = UTY.GetChildObject(___toggleParent, $"{item}/Result");//.GetComponent<UILabel>();
-                var title = UTY.GetChildObject(___toggleParent, $"{item}/Title");//.GetComponent<UILabel>();
+                var result = UTY.GetChildObject(___toggleParent, $"{item}/Result"); //.GetComponent<UILabel>();
+                var title = UTY.GetChildObject(___toggleParent, $"{item}/Title"); //.GetComponent<UILabel>();
 
                 var resultLoc = result.AddComponent<Localize>();
                 resultLoc.SetTerm($"SceneNetorareCheck/{item}_Result");
@@ -43,6 +46,59 @@ namespace COM3D2.i18nEx.Core.Hooks
 
             Localize("Toggle_LockUserDraftMaid");
             Localize("Toggle_IsComPlayer");
+        }
+
+        private delegate void TranslateInfo(ref string text);
+
+        [HarmonyPatch(typeof(SystemShortcut), "OnClick_Info")]
+        [HarmonyTranspiler]
+        public static IEnumerable<CodeInstruction> LocalizeInfoText(IEnumerable<CodeInstruction> instructions)
+        {
+            bool hasText = false;
+            foreach (var codeInstruction in instructions)
+            {
+                if (codeInstruction.opcode == OpCodes.Callvirt && codeInstruction.operand is MethodInfo minfo &&
+                    minfo.Name == "get_SysDlg")
+                    hasText = true;
+                else if (hasText)
+                {
+                    hasText = false;
+                    Core.Logger.LogInfo(
+                        $"Got operand: {codeInstruction.opcode} with operand: {codeInstruction.operand}");
+
+                    int index = -1;
+
+                    if (OpCodes.Ldloc_0.Value <= codeInstruction.opcode.Value &&
+                        codeInstruction.opcode.Value <= OpCodes.Ldloc_3.Value)
+                        index = codeInstruction.opcode.Value - OpCodes.Ldloc_0.Value;
+                    else if (codeInstruction.opcode == OpCodes.Ldloc_S || codeInstruction.opcode == OpCodes.Ldloc)
+                        index = (int)codeInstruction.operand;
+
+                    if (index < 0)
+                    {
+                        Core.Logger.LogError($"Failed to patch info text localization! Please report this!");
+                        yield return codeInstruction;
+                        continue;
+                    }
+
+                    yield return new CodeInstruction(OpCodes.Pop);
+                    yield return new CodeInstruction(OpCodes.Ldloca, index);
+                    yield return HarmonyWrapper.EmitDelegate<TranslateInfo>((ref string text) =>
+                    {
+                        if (LocalizationManager.TryGetTranslation("System/GameInfo_Description", out string tl))
+                            text = string.Format(tl, Product.gameTitle, GameUty.GetBuildVersionText(),
+                                                 GameUty.GetGameVersionText(), GameUty.GetLegacyGameVersionText());
+                    });
+                    yield return new CodeInstruction(OpCodes.Call,
+                                                     AccessTools.PropertyGetter(
+                                                         typeof(GameMain), nameof(GameMain.Instance)));
+                    yield return new CodeInstruction(OpCodes.Callvirt,
+                                                     AccessTools.PropertyGetter(
+                                                         typeof(GameMain), nameof(GameMain.SysDlg)));
+                }
+
+                yield return codeInstruction;
+            }
         }
     }
 }

52a9999ae44a42c34a75d9bbea82bf0cdf677575
ghorsington <neigh@coder.horse>
2019-09-23 19:18:50 +0300

    Fix adding unnecessary space in voice file name

--- EngExtract/EngExtract.cs
+++ EngExtract/EngExtract.cs
@@ -183,6 +183,7 @@ namespace EngExtract
                         keySb.Length = 0;
                         valueSb.Length = 0;
                         captureValue = false;
+                        continue;
                     }
 
                     valueSb.Append(c);
